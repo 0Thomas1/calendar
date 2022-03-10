14b651a779cd81ce65a2b80f44340c82171d924a
0Thomas1 <99365833+0Thomas1@users.noreply.github.com>
2022-03-09 17:23:22 -0800

    Update main.py

--- main.py
+++ main.py
@@ -7,21 +7,29 @@ find prime no.s in a range(biggest, smallest, middle)
 """
 
 
-in_number = input("what is your number?")
+in_number = int(input("what is your number?"))
 
 def check_input(num):
     if num //1 != 0:
         print("This is not a prime number.")
 
 def check_prime(num):
-    num_list = [0] * (num + 1)
-    i = 0 
-    while i < len(num_list):
-        num_list[i] = i
+    """
+    check if the input no. is prime or not
+    """
+    #num_list = [0] * (num + 1)
+    i = 2 
+    remain = 1
+    while i < num and remain != 0:
+        remain = num % i
         i += 1
-    #print(num_list)
-    first = 1
-    last = -1
+        print(remain)
+    if remain == 0:
+        print("this is not a prime number.")
+    else:
+        print(str(num) +" is a prime number.")
+    
     
 
-check_prime(20)
+
+check_prime(in_number)
