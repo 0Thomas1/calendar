3d369ac2bdcd97ef3d9b03df682080e760edaeb9
0Thomas1 <99365833+0Thomas1@users.noreply.github.com>
2022-05-08 01:54:59 -0700

    gui for the app

--- GUI.py
+++ GUI.py
@@ -1,6 +1,28 @@
-import gooeypie as gp
+from tkinter import *
+from tkinter import ttk
 
-app = gp.GooeyPieApp('Prime Number App')
 
-app.run
+root = Tk()
+root.geometry('300x200')
+root.resizable(True, True)
+root.title('Prime Number App')
+root.attributes('-topmost', 1)
+root.iconbitmap('/icon.ico')
 
+# label with a specific font
+label = Label(root, text='which function to use')
+
+label.pack()
+
+exit_button = ttk.Button(
+    root,
+    text='Exit',
+    command=lambda: root.quit())
+
+exit_button.pack(
+    ipadx=1,
+    ipady=1,
+    expand=True
+)
+
+root.mainloop()
